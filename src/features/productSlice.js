22c743dabd6bf542737312c85304e8ff4ed4b662
Harsh <harshpachani@gmail.com>
2024-01-05 16:33:45 +0530

    business logic of adding product into the state

---
+++ src/features/productSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const STATUSES = Object.freeze({
+    IDLE: 'idle',
+    ERROR: 'error',
+    LOADING: 'loading',
+});
+
+const initialState = {
+    data : [],
+    status: STATUSES.IDLE,
+    isLoading: true
+}
+
+export const fetchProducts = createAsyncThunk('products/fetch', async () => {
+    const resp = await fetch('https://fakestoreapi.com/products');
+    const data = await resp.json();
+    return data;
+});
+
+const productSlice = createSlice({
+    name: 'product',
+    initialState: initialState,
+    reducers: {
+        setProduct: (state, action) => {
+            state.data = action.payload;
+        },
+        remove: (state, action)=> {
+            return state.filter((item) => item.id !== action.payload);
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProducts.pending, (state, action) => state.status = STATUSES.LOADING)
+            .addCase(fetchProducts.fulfilled, (state, action) => {
+                state.data = action.payload;
+                state.status = STATUSES.IDLE;
+            })
+            .addCase(fetchProducts.rejected, (state, action) => state.status = STATUSES.ERROR);
+    }
+});
+
+export const { setProduct, remove } = productSlice.actions;
+export default productSlice.reducer;
