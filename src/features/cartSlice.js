a09d8353eedabaac2981224bac1cf2801b6cb2b1
Harsh <harshpachani@gmail.com>
2024-01-06 21:11:13 +0530

    Business logic for showing cart products

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -1,19 +1,43 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [];
+const initialState = {
+    cartItems: [],
+    amount: 0,
+    total: 0
+};
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState: initialState,
     reducers: {
-        add: (state, action) => {
-            state.push(action.payload);
+        addCart: (state, action) => {
+            state.cartItems.push(action.payload);
         },
-        remove: (state, action) => {
-            return state.filter(item => item.id !== action.payload);
+        clearCart: (state, action) => {
+            state.cartItems = [];
+        },
+        removeItem: (state, action) => {
+            state.cartItems = state.cartItems.filter((item) => item.id !== action.payload);
+        },
+        increase: (state, action) => {
+            const cartItem = state.cartItems.find(item => item.id === action.payload);
+            cartItem.amount = cartItem.amount+1;
+        },
+        decrease: (state, action) => {
+            const cartItem = state.cartItems.find(item => item.id === action.payload);
+            cartItem.amount = cartItem.amount-1;
+        },
+        calculateTotals: (state) => {
+            let amount = 0, total = 0;
+            state.cartItems.forEach((item) => {
+                amount += item.amount;
+                total += item.amount * item.price;
+            });
+            state.amount = amount;
+            state.total = total;
         }
     }
 })
 
-export const {add, remove} = cartSlice.actions;
+export const {addCart, clearCart, removeItem, increase, decrease, calculateTotals} = cartSlice.actions;
 export default cartSlice.reducer;
