4ebd78163bf490c10255f43b10072ff8a5ddedce
Harsh <harshpachani@gmail.com>
2024-01-06 21:12:40 +0530

    Entry point of the component in Application

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import { store } from './App/store';
-import { Provider, useSelector } from 'react-redux';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import Carts from './components/Carts';
 import Modal from './components/Modal';
+import About from './components/About';
+import { useEffect } from 'react';
+import { calculateTotals } from './features/cartSlice';
+import SingleProduct from './components/SingleProduct';
 
 function App() {
+
+  const {cartItems} = useSelector((store) => store.cart);
+  const {message} = useSelector((store) => store.modal);
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(calculateTotals());  
+  }, [cartItems])
 
   const {isOpen} = useSelector((store) => store.modal);
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Navbar />
-        {isOpen && <Modal/>}
+        {isOpen && <Modal message={message}/>}
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path="/cart" element={<Carts/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/product/:id" element={<SingleProduct/>}/>
         </Routes>
       </BrowserRouter>
     </Provider>
