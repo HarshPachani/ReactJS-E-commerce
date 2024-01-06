b002ac88ec1a6ef5eaf748c54b08877a8a498358
Harsh <harshpachani@gmail.com>
2024-01-06 21:13:28 +0530

    store for registering all the slices

--- src/App/store.js
+++ src/App/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../features/cartSlice';
 import productReducer from '../features/productSlice';
 import modalReducer from '../features/modalSlice';
+import singleProductReducer from '../features/singleProductSlice';
 
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
         product: productReducer,
-        modal: modalReducer
+        modal: modalReducer,
+        singleProduct: singleProductReducer,
     }
 });
