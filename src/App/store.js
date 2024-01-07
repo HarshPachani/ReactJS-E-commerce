c1f22a50e87a968a97fc9af59c1557ea87f0a28c
Harsh <harshpachani@gmail.com>
2024-01-07 16:23:24 +0530

    Registered categories slice

--- src/App/store.js
+++ src/App/store.js
@@ -3,12 +3,13 @@ import cartReducer from '../features/cartSlice';
 import productReducer from '../features/productSlice';
 import modalReducer from '../features/modalSlice';
 import singleProductReducer from '../features/singleProductSlice';
-
+import categoryReducer from '../features/categorySlice';
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
         product: productReducer,
         modal: modalReducer,
         singleProduct: singleProductReducer,
+        category: categoryReducer,
     }
 });
