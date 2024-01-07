290ca7f2d29bdf30d0fc8b9c9ec67a6f4abe7c58
Harsh <harshpachani@gmail.com>
2024-01-07 16:34:48 +0530

    Added z-index property

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,7 @@ import React from 'react'
 import img from '../check.png';
 function Modal({message}) {
   return (
-    <div className='flex justify-center align-baseline sticky top-0'>
+    <div className='flex justify-center align-baseline sticky top-0 z-50'>
       <div className='ml-20 mr-20 p-3 border-b-4 border-[#9f2089] rounded bg-white text-black font-bold flex justify-center items-center'>
           {message } <img src={img} alt="" style={{width: '20px', marginLeft: '5px'}}/>
       </div>
