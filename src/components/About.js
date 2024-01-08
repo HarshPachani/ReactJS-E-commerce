65e5c8802e599bd797d9591936ad49ef4c91fbc7
Harsh <harshpachani@gmail.com>
2024-01-08 14:31:17 +0530

    Added about E-comm section

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,41 @@ import React from 'react'
 
 function About() {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem ullam aliquid accusantium provident rerum. Beatae expedita suscipit mollitia doloremque obcaecati quaerat voluptatum blanditiis. Id asperiores repellat mollitia, dolores laboriosam laudantium odit esse, cupiditate voluptates atque fugiat, ut molestiae in quidem. Natus consequuntur iste asperiores necessitatibus animi aperiam exercitationem ipsum perferendis repellendus sint rerum quaerat, quis, molestias fugiat sed? Praesentium delectus ea rerum asperiores eaque vitae repellendus ratione libero eum dignissimos neque sint voluptate minus reprehenderit unde a, nulla animi suscipit commodi iste. Facere exercitationem amet commodi deleniti, illo nesciunt tempora dolore ratione excepturi recusandae? Esse delectus repellendus dignissimos culpa, placeat, quia molestiae, repellat harum id libero laborum porro ipsam. Asperiores ex quia distinctio atque ab cum illo, dolorem quibusdam unde quam. Error sequi doloremque eum possimus dicta quasi commodi similique harum nesciunt deleniti repellendus facilis, autem, accusamus vitae quis vero. Hic illo non reprehenderit quibusdam aliquam temporibus provident cupiditate quo! Eligendi aperiam voluptatum reprehenderit quibusdam velit consectetur harum voluptas repellat itaque. Culpa ipsam maxime at atque inventore distinctio cum, asperiores deserunt delectus. Incidunt neque, reiciendis inventore nostrum sapiente minima nam modi est sequi vitae ab illo quisquam, consectetur at ullam ratione quae magnam beatae tenetur magni libero debitis esse alias. Dicta doloribus ipsum aperiam reiciendis! Laudantium voluptates commodi magnam, amet esse deleniti dolore non quae alias consectetur quod excepturi error aut eveniet cumque nam. Mollitia dolor illum exercitationem debitis deserunt voluptatem placeat sunt similique enim voluptatibus tempora, quibusdam repudiandae quis reprehenderit ad ex quae natus? Quas, ea nesciunt nulla possimus iusto in nisi, cumque eius excepturi velit ab culpa ex vero architecto quam libero perferendis laboriosam sequi magnam veniam nihil eos, accusantium aliquid! Ad perferendis impedit illum praesentium, aperiam beatae nulla. Architecto nisi iusto magnam deleniti vel autem praesentium excepturi laboriosam totam exercitationem, sint commodi dolores natus placeat ipsum quas, ad, ut mollitia accusantium? Nulla magnam molestiae cupiditate asperiores adipisci consequuntur commodi nemo, ea non ipsum sunt fuga aperiam necessitatibus, quidem expedita odit dolores omnis! Ipsa facere delectus deleniti sunt. Aliquid, fugiat. Blanditiis adipisci at expedita quod esse similique vel vitae ipsum! Ab commodi accusantium illo expedita cupiditate corporis ipsum.</div>
+    <div className='m-20'>
+      <div className='mb-10 text-center'>
+        <p className='font-bold text-2xl ml-5'>Welcome to E-Comm – Your Ultimate Shopping Destination!</p>
+      </div>
+
+      <div>
+        <h1 className='font-bold text-2xl'>About E-comm</h1>
+        <p className='ml-5 mt-2'>At E-Comm, we believe in providing a seamless and enjoyable online shopping experience. E-comm is dedicated to curating a diverse range of high-quality products and bringing them to your fingertips. Whether you're a fashion enthusiast, a tech guru, or someone looking for unique finds, we've got you covered.</p>
+      </div>
+
+      <div className='mt-8'>
+        <h1 className='font-bold text-2xl'>Technology Stack</h1>
+        <p className='ml-5 mt-2'>E-Comm is not just an ordinary e-commerce platform; it's a blend of cutting-edge technology and user-centric design. Our website is crafted using ReactJS for a smooth and responsive interface, TailwindCSS for a visually appealing layout, and Redux-toolkit to ensure efficient state management. We've leveraged the power of these technologies to create a platform that's not only visually stunning but also functionally robust.</p>
+      </div>
+
+      <div className='mt-8'>
+        <h1 className='font-bold text-2xl'>What Sets E-comm Apart</h1>
+        <ol className='ml-2'>
+          <li className='mt-2 mb-2'>
+            <h1 className='font-bold text-xl'>User-Friendly Interface:</h1>
+            <p className='ml-5 mt-1'>We understand the importance of a user-friendly interface. That's why we've invested time and effort into designing an intuitive website that makes your shopping experience effortless.</p>
+          </li>
+
+          <li className='mt-2 mb-2'>
+            <h1 className='font-bold text-xl'>Diverse Product Range:</h1>
+            <p className='ml-5 mt-1'> E-Comm boasts a diverse collection of products, spanning various categories to cater to your unique needs and preferences. From the latest fashion trends to cutting-edge gadgets, we have it all.</p>
+          </li>
+
+          <li className='mt-2 mb-2'>
+            <h1 className='font-bold text-xl'>Seamless Shopping Journey:</h1>
+            <p className='ml-5 mt-1'>Explore our website with ease. From browsing through product categories to detailed product descriptions, adding items to your cart, and securely checking out – we've streamlined every step to ensure a hassle-free </p>
+          </li>
+        </ol>
+      </div>
+    </div>
   )
 }
 
